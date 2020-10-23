5692d11de43ee522483e7c3d71a2999271e7c869
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-23 10:21:18 +0200

    Add WorkoutEntity

---
+++ app/src/main/java/com/kobietka/trainingtimer/data/WorkoutEntity.kt
@@ -0,0 +1,13 @@
+package com.kobietka.trainingtimer.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "workouts")
+data class WorkoutEntity(@PrimaryKey(autoGenerate = true) val id: Int?,
+                         @ColumnInfo val name: String,
+                         @ColumnInfo val restTime: Int,
+                         @ColumnInfo val creationDate: String,
+                         @ColumnInfo val totalRepetitionCount: Int,
+                         @ColumnInfo val totalTimeCount: Int)
