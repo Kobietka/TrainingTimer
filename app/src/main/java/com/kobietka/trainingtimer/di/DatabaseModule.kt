cec4f18a19fd43305c47ee4ad2ad809172dafa52
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-23 11:59:00 +0200

    Add DatabaseModule class

---
+++ app/src/main/java/com/kobietka/trainingtimer/di/DatabaseModule.kt
@@ -0,0 +1,36 @@
+package com.kobietka.trainingtimer.di
+
+import android.content.Context
+import androidx.room.Room
+import com.kobietka.trainingtimer.data.AppDatabase
+import com.kobietka.trainingtimer.data.ExerciseDao
+import com.kobietka.trainingtimer.data.WorkoutDao
+import dagger.Module
+import dagger.Provides
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module(includes = [ApplicationModule::class])
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@Named("ApplicationContext") context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "app_database"
+        ).fallbackToDestructiveMigration().build()
+    }
+
+    @Provides
+    fun provideWorkoutDao(appDatabase: AppDatabase): WorkoutDao {
+        return appDatabase.workoutDao()
+    }
+
+    @Provides
+    fun provideExerciseDao(appDatabase: AppDatabase): ExerciseDao {
+        return appDatabase.exerciseDao()
+    }
+
+}
