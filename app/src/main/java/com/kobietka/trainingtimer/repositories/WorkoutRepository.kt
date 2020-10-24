b23bf755d72cf6e34940dfb69aec9b8e500d4b3d
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-24 11:05:06 +0200

    Add WorkoutRepository class

---
+++ app/src/main/java/com/kobietka/trainingtimer/repositories/WorkoutRepository.kt
@@ -0,0 +1,29 @@
+package com.kobietka.trainingtimer.repositories
+
+import com.kobietka.trainingtimer.data.WorkoutDao
+import com.kobietka.trainingtimer.data.WorkoutEntity
+import io.reactivex.Completable
+import io.reactivex.Observable
+import javax.inject.Inject
+
+
+class WorkoutRepository
+@Inject constructor(private val workoutDao: WorkoutDao){
+
+    fun getAllWorkouts(): Observable<List<WorkoutEntity>> {
+        return workoutDao.getAllWorkouts()
+    }
+
+    fun insertWorkout(workoutEntity: WorkoutEntity){
+        workoutDao.insertWorkout(workoutEntity)
+    }
+
+    fun getById(id: Int): Observable<WorkoutEntity> {
+        return workoutDao.getById(id)
+    }
+
+    fun deleteById(id: Int): Completable {
+        return workoutDao.deleteById(id)
+    }
+
+}
