ca818a717fb0e3dcdba3375d8d079be77f00df7c
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-24 12:15:06 +0200

    Add App class to manifest

--- app/src/main/java/com/kobietka/trainingtimer/App.kt
+++ app/src/main/java/com/kobietka/trainingtimer/App.kt
@@ -5,7 +5,7 @@ import com.kobietka.trainingtimer.di.ApplicationComponent
 import com.kobietka.trainingtimer.di.ApplicationModule
 import com.kobietka.trainingtimer.di.DaggerApplicationComponent
 
-class App: Application() {
+class App : Application() {
 
     companion object {
         lateinit var applicationComponent: ApplicationComponent
