d7dd5ec858b71f582cc0e62bfcca17a07ebcab1d
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-25 14:10:37 +0100

    Add inject function for MainFragment.kt

--- app/src/main/java/com/kobietka/trainingtimer/di/PresentationComponent.kt
+++ app/src/main/java/com/kobietka/trainingtimer/di/PresentationComponent.kt
@@ -1,10 +1,12 @@
 package com.kobietka.trainingtimer.di
 
 import com.kobietka.trainingtimer.presentaion.ui.MainActivity
+import com.kobietka.trainingtimer.presentaion.ui.fragmentmainmenu.MainFragment
 import dagger.Subcomponent
 
 
 @Subcomponent(modules = [PresentationModule::class])
 interface PresentationComponent {
     fun inject(mainActivity: MainActivity)
+    fun inject(mainFragment: MainFragment)
 }
