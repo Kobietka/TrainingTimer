db7fcd514de64e5d17d72991c06cd35a1f659bff
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-26 09:55:03 +0100

    Add ExerciseAddFragment class

---
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/fragmentaddexercise/ExerciseAddFragment.kt
@@ -0,0 +1,19 @@
+package com.kobietka.trainingtimer.presentaion.ui.fragmentaddexercise
+
+import android.os.Bundle
+import android.view.View
+import com.kobietka.trainingtimer.R
+import com.kobietka.trainingtimer.presentaion.common.BaseFragment
+
+
+class ExerciseAddFragment : BaseFragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.fragment_add_exercise
+    }
+
+}
