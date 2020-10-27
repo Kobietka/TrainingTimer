29facc22ef93c24d18cb9e02a495abd4c6c51050
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-27 09:06:21 +0100

    Visual change

--- app/src/main/java/com/kobietka/trainingtimer/di/PresentationModule.kt
+++ app/src/main/java/com/kobietka/trainingtimer/di/PresentationModule.kt
@@ -1,16 +1,23 @@
 package com.kobietka.trainingtimer.di
 
 import android.content.Context
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
 import com.kobietka.trainingtimer.presentaion.common.BaseActivity
 import com.kobietka.trainingtimer.presentaion.common.BaseFragment
 import com.kobietka.trainingtimer.presentaion.ui.MainActivity
+import com.kobietka.trainingtimer.presentaion.ui.rvs.ExercisesAdapter
+import com.kobietka.trainingtimer.presentaion.viewmodels.ExerciseViewModel
+import com.kobietka.trainingtimer.repositories.ExerciseRepository
 import dagger.Module
 import dagger.Provides
 import javax.inject.Named
 
 
 @Module
-class PresentationModule(private val activity: BaseActivity? = null, private val baseFragment: BaseFragment? = null) {
+class PresentationModule(private val activity: BaseActivity? = null,
+                         private val baseFragment: BaseFragment? = null) {
 
     @Provides
     @Named("ActivityContext")
