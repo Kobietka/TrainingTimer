aa03e055ae0b635134040c1d18d883887489dc68
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-27 20:12:03 +0100

    Add inject function for EditExerciseFragment

--- app/src/main/java/com/kobietka/trainingtimer/di/PresentationComponent.kt
+++ app/src/main/java/com/kobietka/trainingtimer/di/PresentationComponent.kt
@@ -2,6 +2,7 @@ package com.kobietka.trainingtimer.di
 
 import com.kobietka.trainingtimer.presentaion.ui.MainActivity
 import com.kobietka.trainingtimer.presentaion.ui.fragmentaddexercise.ExerciseAddFragment
+import com.kobietka.trainingtimer.presentaion.ui.fragmenteditexercise.EditExerciseFragment
 import com.kobietka.trainingtimer.presentaion.ui.fragmentexercises.ExercisesFragment
 import com.kobietka.trainingtimer.presentaion.ui.fragmentmainmenu.MainFragment
 import dagger.Subcomponent
@@ -13,4 +14,5 @@ interface PresentationComponent {
     fun inject(mainFragment: MainFragment)
     fun inject(exerciseAddFragment: ExerciseAddFragment)
     fun inject(exercisesFragment: ExercisesFragment)
+    fun inject(editExerciseFragment: EditExerciseFragment)
 }
