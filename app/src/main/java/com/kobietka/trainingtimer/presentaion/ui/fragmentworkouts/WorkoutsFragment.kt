8dcb72ca6f44e4967b6e039fce10d29531db0aaf
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-28 17:40:15 +0100

    Add WorkoutsFragment class

---
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/fragmentworkouts/WorkoutsFragment.kt
@@ -0,0 +1,29 @@
+package com.kobietka.trainingtimer.presentaion.ui.fragmentworkouts
+
+import android.os.Bundle
+import android.view.View
+import com.kobietka.trainingtimer.R
+import com.kobietka.trainingtimer.presentaion.common.BaseFragment
+import com.kobietka.trainingtimer.presentaion.ui.fragmentmainmenu.MainFragment
+import kotlinx.android.synthetic.main.fragment_workouts.*
+
+
+class WorkoutsFragment : BaseFragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        presentationComponent.inject(this)
+
+        fragment_workouts_back_arrow.setOnClickListener {
+            activity!!.supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, MainFragment())
+                .commit()
+        }
+
+    }
+
+    override fun getLayout(): Int {
+        return R.id.fragment_workouts
+    }
+
+}
