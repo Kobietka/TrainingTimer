45fd47493b29621de33b93da028fb127b1fb7199
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-28 22:45:39 +0100

    Add initialization of recycler view

--- app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/fragmentworkouts/WorkoutsFragment.kt
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/fragmentworkouts/WorkoutsFragment.kt
@@ -2,17 +2,41 @@ package com.kobietka.trainingtimer.presentaion.ui.fragmentworkouts
 
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.kobietka.trainingtimer.R
 import com.kobietka.trainingtimer.presentaion.common.BaseFragment
+import com.kobietka.trainingtimer.presentaion.ui.fragmentaddworkout.WorkoutAddFragment
 import com.kobietka.trainingtimer.presentaion.ui.fragmentmainmenu.MainFragment
+import com.kobietka.trainingtimer.presentaion.ui.rvs.WorkoutsAdapter
 import kotlinx.android.synthetic.main.fragment_workouts.*
+import javax.inject.Inject
 
 
 class WorkoutsFragment : BaseFragment() {
 
+    @Inject lateinit var adapter: WorkoutsAdapter
+    lateinit var recyclerView: RecyclerView
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         presentationComponent.inject(this)
+
+        recyclerView = view.findViewById(R.id.fragment_workouts_rv)
+        recyclerView.layoutManager = LinearLayoutManager(
+            activity,
+            RecyclerView.VERTICAL,
+            false
+        )
+
+        adapter.setLifeCycleOwner(viewLifecycleOwner)
+        recyclerView.adapter = adapter
+
+        fragment_workouts_add.setOnClickListener {
+            activity!!.supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, WorkoutAddFragment())
+                .commit()
+        }
 
         fragment_workouts_back_arrow.setOnClickListener {
             activity!!.supportFragmentManager.beginTransaction()
