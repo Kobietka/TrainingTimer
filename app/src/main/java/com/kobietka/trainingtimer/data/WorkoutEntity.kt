b48519afd0a77abf87847612e8d0697b2419ccdb
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-28 18:46:08 +0100

    Add exercises field

--- app/src/main/java/com/kobietka/trainingtimer/data/WorkoutEntity.kt
+++ app/src/main/java/com/kobietka/trainingtimer/data/WorkoutEntity.kt
@@ -8,6 +8,7 @@ import androidx.room.PrimaryKey
 data class WorkoutEntity(@PrimaryKey(autoGenerate = true) val id: Int?,
                          @ColumnInfo val name: String,
                          @ColumnInfo val restTime: Int,
+                         @ColumnInfo val exercises: MutableList<Int>,
                          @ColumnInfo val creationDate: String,
                          @ColumnInfo val totalRepetitionCount: Int,
                          @ColumnInfo val totalTimeCount: Int)
