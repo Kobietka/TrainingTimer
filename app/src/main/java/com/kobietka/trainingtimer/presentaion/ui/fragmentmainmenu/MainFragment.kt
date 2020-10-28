a0914413f8e1046eb2bd1b7b9f6b31f92a62a92e
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-28 18:46:47 +0100

    Add onClickListener to open WorkoutsFragment

--- app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/fragmentmainmenu/MainFragment.kt
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/fragmentmainmenu/MainFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import com.kobietka.trainingtimer.R
 import com.kobietka.trainingtimer.presentaion.common.BaseFragment
 import com.kobietka.trainingtimer.presentaion.ui.fragmentexercises.ExercisesFragment
+import com.kobietka.trainingtimer.presentaion.ui.fragmentworkouts.WorkoutsFragment
 import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : BaseFragment() {
@@ -12,6 +13,12 @@ class MainFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         presentationComponent.inject(this)
+
+        fragment_main_workouts_elements.setOnClickListener {
+            activity!!.supportFragmentManager.beginTransaction()
+                .replace(R.id.main_container, WorkoutsFragment())
+                .commit()
+        }
 
         fragment_main_exercises_element.setOnClickListener {
             activity!!.supportFragmentManager.beginTransaction()
