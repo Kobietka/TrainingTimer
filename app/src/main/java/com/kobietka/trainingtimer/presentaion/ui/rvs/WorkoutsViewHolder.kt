161502fa67aac1d697231dc6b378cb270aab5bd7
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-28 18:51:37 +0100

    Add WorkoutsViewHolder class

---
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/rvs/WorkoutsViewHolder.kt
@@ -0,0 +1,32 @@
+package com.kobietka.trainingtimer.presentaion.ui.rvs
+
+import android.view.View
+import android.widget.TextView
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.RecyclerView
+import com.kobietka.trainingtimer.R
+import com.kobietka.trainingtimer.presentaion.viewmodels.WorkoutsViewModel
+
+
+class WorkoutsViewHolder(itemView: View,
+                         val viewModel: WorkoutsViewModel,
+                         private val lifecycleOwner: LifecycleOwner) : RecyclerView.ViewHolder(itemView) {
+
+
+    fun onAttach(){
+        viewModel.name().observe(lifecycleOwner, {
+            itemView.findViewById<TextView>(R.id.fragment_workout_entry_name)
+                .text = it
+        })
+
+        viewModel.numberOfExercises().observe(lifecycleOwner, {
+            itemView.findViewById<TextView>(R.id.fragment_workout_entry_count)
+        })
+    }
+
+    fun onDetach(){
+        viewModel.name().removeObservers(lifecycleOwner)
+        viewModel.numberOfExercises().removeObservers(lifecycleOwner)
+    }
+
+}
