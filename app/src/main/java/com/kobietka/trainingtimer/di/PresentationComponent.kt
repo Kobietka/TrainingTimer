6626287b82ff872639c57ed0c688aaaffca6744e
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-10-31 11:42:13 +0100

    Add inject functions

--- app/src/main/java/com/kobietka/trainingtimer/di/PresentationComponent.kt
+++ app/src/main/java/com/kobietka/trainingtimer/di/PresentationComponent.kt
@@ -3,7 +3,9 @@ package com.kobietka.trainingtimer.di
 import com.kobietka.trainingtimer.presentaion.ui.MainActivity
 import com.kobietka.trainingtimer.presentaion.ui.fragmentaddexercise.ExerciseAddFragment
 import com.kobietka.trainingtimer.presentaion.ui.fragmentaddworkout.WorkoutAddFragment
+import com.kobietka.trainingtimer.presentaion.ui.fragmentchooseexercise.ChooseExerciseFragment
 import com.kobietka.trainingtimer.presentaion.ui.fragmenteditexercise.EditExerciseFragment
+import com.kobietka.trainingtimer.presentaion.ui.fragmenteditworkout.EditWorkoutFragment
 import com.kobietka.trainingtimer.presentaion.ui.fragmentexercises.ExercisesFragment
 import com.kobietka.trainingtimer.presentaion.ui.fragmentmainmenu.MainFragment
 import com.kobietka.trainingtimer.presentaion.ui.fragmentworkouts.WorkoutsFragment
@@ -19,4 +21,6 @@ interface PresentationComponent {
     fun inject(editExerciseFragment: EditExerciseFragment)
     fun inject(workoutsFragment: WorkoutsFragment)
     fun inject(workoutAddFragment: WorkoutAddFragment)
+    fun inject(editWorkoutFragment: EditWorkoutFragment)
+    fun inject(chooseExerciseFragment: ChooseExerciseFragment)
 }
