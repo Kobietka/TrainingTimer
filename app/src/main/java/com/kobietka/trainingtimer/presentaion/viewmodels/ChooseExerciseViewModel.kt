e352afd703b7ebef59dc06373335c8c4a4d249f5
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-02 21:04:45 +0100

    Add new subscribe methods

--- app/src/main/java/com/kobietka/trainingtimer/presentaion/viewmodels/ChooseExerciseViewModel.kt
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/viewmodels/ChooseExerciseViewModel.kt
@@ -1,11 +1,15 @@
 package com.kobietka.trainingtimer.presentaion.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.kobietka.trainingtimer.data.WorkoutRelation
 import com.kobietka.trainingtimer.models.ClickId
 import com.kobietka.trainingtimer.models.EventType
 import com.kobietka.trainingtimer.models.MeasurementType
+import com.kobietka.trainingtimer.models.WorkoutAddExerciseEvent
 import com.kobietka.trainingtimer.repositories.ExerciseRepository
+import com.kobietka.trainingtimer.repositories.WorkoutRelationRepository
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
@@ -14,16 +18,41 @@ import io.reactivex.subjects.Subject
 import javax.inject.Inject
 
 class ChooseExerciseViewModel
-@Inject constructor(private val repository: ExerciseRepository,
-                    private val eventSubject: Subject<EventType>){
+@Inject constructor(private val workoutRelationRepository: WorkoutRelationRepository,
+                    private val exerciseRepository: ExerciseRepository,
+                    private val workoutIdSender: Subject<Int>,
+                    private val launchEvents: Subject<EventType>){
 
     private val compositeDisposable = CompositeDisposable()
     private val addClicks = BehaviorSubject.create<ClickId>().toSerialized()
+    private val workoutIds = BehaviorSubject.create<Int>().toSerialized()
     private val ids = BehaviorSubject.create<Int>().toSerialized()
 
     private val _name = MutableLiveData<String>()
     private val _measurementValue = MutableLiveData<Int>()
     private val _measurementType = MutableLiveData<MeasurementType>()
+
+    init {
+        compositeDisposable.add(
+            workoutIdSender.subscribe(this::switchWorkoutId)
+        )
+
+        compositeDisposable.add(
+            ids.subscribe(this::loadExercise)
+        )
+
+        addClicks.withLatestFrom(ids, workoutIds, { clickId, exerciseId, workoutId ->
+            workoutRelationRepository.insert(WorkoutRelation(null, workoutId, exerciseId))
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    launchEvents.onNext(EventType(clickId, workoutId))
+                }
+
+        }).subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe()
+    }
 
     fun measurementValue(): LiveData<Int> {
         return _measurementValue
@@ -37,18 +66,21 @@ class ChooseExerciseViewModel
         return _measurementType
     }
 
+    private fun switchWorkoutId(id: Int){
+        workoutIds.onNext(id)
+    }
+
     fun onAddClick(){
-        addClicks.onNext(ClickId.AddExercise)
+        addClicks.onNext(ClickId.EditWorkoutFromChoosing)
     }
 
     fun switchId(id: Int){
-        loadExercise(id)
         ids.onNext(id)
     }
 
     private fun loadExercise(id: Int) {
         compositeDisposable.add(
-            repository.getById(id)
+            exerciseRepository.getById(id)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
