814136fd781e6f2655c578b88c65ed236b615044
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-02 21:05:16 +0100

    Add new subscribe methods

--- app/src/main/java/com/kobietka/trainingtimer/presentaion/viewmodels/EditWorkoutViewModel.kt
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/viewmodels/EditWorkoutViewModel.kt
@@ -1,12 +1,16 @@
 package com.kobietka.trainingtimer.presentaion.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.kobietka.trainingtimer.models.ClickId
 import com.kobietka.trainingtimer.models.EventType
 import com.kobietka.trainingtimer.models.MeasurementType
+import com.kobietka.trainingtimer.presentaion.ui.rvs.EditWorkoutAdapter
 import com.kobietka.trainingtimer.repositories.ExerciseRepository
+import com.kobietka.trainingtimer.repositories.WorkoutRelationRepository
 import com.kobietka.trainingtimer.repositories.WorkoutRepository
+import com.mikepenz.fastadapter.dsl.genericFastAdapter
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
@@ -16,28 +20,29 @@ import javax.inject.Inject
 
 
 class EditWorkoutViewModel
-@Inject constructor(private val workoutRepository: WorkoutRepository,
-                    private val exerciseRepository: ExerciseRepository,
-                    private val eventSubject: Subject<EventType>){
+@Inject constructor(private val workoutRelationRepository: WorkoutRelationRepository,
+                    private val exerciseRepository: ExerciseRepository){
 
     private val compositeDisposable = CompositeDisposable()
-    private val ids = BehaviorSubject.create<Int>().toSerialized()
-    private val addClicks = BehaviorSubject.create<ClickId>().toSerialized()
-    private var workoutId = 0
-
-    private val _name = MutableLiveData<String>()
-    private val _restTime = MutableLiveData<Int>()
+    private val relationIds = BehaviorSubject.create<Int>().toSerialized()
+    private val deleteClicks = BehaviorSubject.create<ClickId>().toSerialized()
 
     private val _exerciseName = MutableLiveData<String>()
     private val _exerciseValue = MutableLiveData<Int>()
     private val _measurementType = MutableLiveData<MeasurementType>()
 
-    fun name(): LiveData<String> {
-        return _name
-    }
+    init {
+        compositeDisposable.add(
+            relationIds.subscribe(this::loadRelation)
+        )
 
-    fun restTime(): LiveData<Int> {
-        return _restTime
+        compositeDisposable.add(
+            deleteClicks.withLatestFrom(relationIds, { clickId, relationId ->
+                workoutRelationRepository.deleteById(relationId)
+                    .subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe()
+            }).subscribe()
+        )
+
     }
 
     fun exerciseValue(): LiveData<Int> {
@@ -52,23 +57,21 @@ class EditWorkoutViewModel
         return _measurementType
     }
 
-    fun setId(id: Int){
-        workoutId = id
-    }
-
     fun switchId(id: Int){
-        ids.onNext(id)
-        loadExercise(id)
+        relationIds.onNext(id)
     }
 
-    fun loadWorkout(id: Int){
+    fun onDeleteClick(){
+        deleteClicks.onNext(ClickId.Delete)
+    }
+
+    private fun loadRelation(id: Int){
         compositeDisposable.add(
-            workoutRepository.getById(id)
-                .subscribeOn(Schedulers.io())
+            workoutRelationRepository.getById(id)
+                .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
-                    _name.value = it.name
-                    _restTime.value = it.restTime
+                    loadExercise(it)
                 }
         )
     }
@@ -76,7 +79,7 @@ class EditWorkoutViewModel
     private fun loadExercise(id: Int){
         compositeDisposable.add(
             exerciseRepository.getById(id)
-                .subscribeOn(Schedulers.io())
+                .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
                     _exerciseName.value = it.name
