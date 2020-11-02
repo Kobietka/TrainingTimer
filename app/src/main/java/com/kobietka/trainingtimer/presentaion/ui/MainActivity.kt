54340921e86c33e38bb531e54a61a226eddc378e
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-02 21:01:53 +0100

    Add new event actions

--- app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/MainActivity.kt
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/MainActivity.kt
@@ -2,11 +2,15 @@ package com.kobietka.trainingtimer.presentaion.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.kobietka.trainingtimer.R
 import com.kobietka.trainingtimer.models.ClickId
 import com.kobietka.trainingtimer.models.EventType
+import com.kobietka.trainingtimer.models.WorkoutAddExerciseEvent
 import com.kobietka.trainingtimer.presentaion.common.BaseActivity
 import com.kobietka.trainingtimer.presentaion.ui.fragmentaddexercise.ExerciseAddFragment
+import com.kobietka.trainingtimer.presentaion.ui.fragmentaddworkout.WorkoutAddFragment
+import com.kobietka.trainingtimer.presentaion.ui.fragmentchooseexercise.ChooseExerciseFragment
 import com.kobietka.trainingtimer.presentaion.ui.fragmenteditexercise.EditExerciseFragment
 import com.kobietka.trainingtimer.presentaion.ui.fragmenteditworkout.EditWorkoutFragment
 import com.kobietka.trainingtimer.presentaion.ui.fragmentmainmenu.MainFragment
@@ -16,8 +20,7 @@ import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
 
-    @Inject
-    lateinit var launchEvents: Subject<EventType>
+    @Inject lateinit var launchEvents: Subject<EventType>
     private val compositeDisposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,9 +43,38 @@ class MainActivity : BaseActivity() {
                 .replace(R.id.main_container, EditExerciseFragment())
                 .commit()
         } else if(eventType.clickId == ClickId.EditWorkout){
-            this.supportFragmentManager.beginTransaction()
-                .replace(R.id.main_container, EditWorkoutFragment())
-                .commit()
+            val fragment = supportFragmentManager.findFragmentByTag("edit_workout_fragment")
+            if(fragment == null){
+                this.supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_container, EditWorkoutFragment(), "edit_workout_fragment")
+                    .commit()
+            } else {
+                this.supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_container, fragment, "edit_workout_fragment")
+                    .commit()
+            }
+        } else if(eventType.clickId == ClickId.AddExercise) {
+            val fragment = supportFragmentManager.findFragmentByTag("add_exercise_fragment")
+            if (fragment == null) {
+                this.supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_container, ChooseExerciseFragment(), "add_exercise_fragment")
+                    .commit()
+            } else {
+                this.supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_container, fragment, "add_exercise_fragment")
+                    .commit()
+            }
+        } else if(eventType.clickId == ClickId.EditWorkoutFromChoosing){
+            val fragment = supportFragmentManager.findFragmentByTag("edit_workout_fragment")
+            if(fragment == null){
+                this.supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_container, EditWorkoutFragment(), "edit_workout_fragment")
+                    .commit()
+            } else {
+                this.supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_container, fragment, "edit_workout_fragment")
+                    .commit()
+            }
         }
     }
 
