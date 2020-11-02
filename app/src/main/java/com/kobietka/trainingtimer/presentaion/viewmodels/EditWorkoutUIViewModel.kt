99f75d0efda6e4f56753b9d4d6757626a5203ff7
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-02 21:03:52 +0100

    Add EditWorkoutUIViewModel class

---
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/viewmodels/EditWorkoutUIViewModel.kt
@@ -0,0 +1,93 @@
+package com.kobietka.trainingtimer.presentaion.viewmodels
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.kobietka.trainingtimer.models.ClickId
+import com.kobietka.trainingtimer.models.EventType
+import com.kobietka.trainingtimer.presentaion.ui.rvs.EditWorkoutAdapter
+import com.kobietka.trainingtimer.repositories.WorkoutRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.Subject
+import javax.inject.Inject
+
+
+class EditWorkoutUIViewModel
+@Inject constructor(private val workoutRepository: WorkoutRepository, private val launchEvents: Subject<EventType>){
+
+    val compositeDisposable = CompositeDisposable()
+    val ids = BehaviorSubject.create<Int>().toSerialized()
+    private val names = BehaviorSubject.create<String>().toSerialized()
+    private val restTimes = BehaviorSubject.create<Int>().toSerialized()
+    private val addExercise = BehaviorSubject.create<ClickId>().toSerialized()
+
+    private val _name = MutableLiveData<String>()
+    private val _restTime = MutableLiveData<Int>()
+
+    init {
+        compositeDisposable.add(
+            ids.subscribe(this::loadWorkout)
+        )
+
+        addExercise.withLatestFrom(ids, { clickId, workoutId ->
+            launchEvents.onNext(EventType(clickId, workoutId))
+        }).subscribe()
+
+        restTimes.withLatestFrom(ids, names, { restTime, id, name ->
+            saveWorkout(id, name, restTime)
+        }).subscribe()
+    }
+
+    fun name(): LiveData<String> {
+        return _name
+    }
+
+    fun restTime(): LiveData<Int> {
+        return _restTime
+    }
+
+    fun switchId(id: Int){
+        ids.onNext(id)
+    }
+
+    fun onAddExerciseClick(){
+        addExercise.onNext(ClickId.AddExercise)
+    }
+
+    fun onSaveClick(name: String, restTime: Int){
+        names.onNext(name)
+        restTimes.onNext(restTime)
+    }
+
+    private fun saveWorkout(workoutId: Int, name: String, restTime: Int){
+        compositeDisposable.add(
+            workoutRepository.updateName(workoutId, name)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe()
+        )
+
+        compositeDisposable.add(
+            workoutRepository.updateRestTime(workoutId, restTime)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe()
+        )
+    }
+
+    private fun loadWorkout(id: Int){
+        compositeDisposable.add(
+            workoutRepository.getById(id)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    _name.value = it.name
+                    _restTime.value = it.restTime
+                }
+        )
+    }
+
+}
