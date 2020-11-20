fabaa4937252465328d5640cee5a077ef9082db8
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-20 20:05:15 +0100

    Add CompletedGoal entity

---
+++ app/src/main/java/com/kobietka/trainingtimer/data/CompletedGoal.kt
@@ -0,0 +1,16 @@
+package com.kobietka.trainingtimer.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.kobietka.trainingtimer.models.MeasurementType
+
+@Entity(tableName = "completedGoal")
+data class CompletedGoal(@PrimaryKey(autoGenerate = true) val id: Int?,
+                         @ColumnInfo val name: String,
+                         @ColumnInfo val goal: Int,
+                         @ColumnInfo val type: MeasurementType,
+                         @ColumnInfo val workoutId: Int?,
+                         @ColumnInfo val creationDate: String,
+                         @ColumnInfo val completeDate: String
+)
