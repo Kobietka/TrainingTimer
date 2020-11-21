893a5c9962c5e45bc33f84a71af307c4a9c05faa
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-21 10:11:02 +0100

    Add methods to provide repositories

--- app/src/main/java/com/kobietka/trainingtimer/di/RepositoryModule.kt
+++ app/src/main/java/com/kobietka/trainingtimer/di/RepositoryModule.kt
@@ -1,7 +1,11 @@
 package com.kobietka.trainingtimer.di
 
+import com.kobietka.trainingtimer.data.ActiveGoalDao
+import com.kobietka.trainingtimer.data.CompletedGoalDao
 import com.kobietka.trainingtimer.data.ExerciseDao
 import com.kobietka.trainingtimer.data.WorkoutDao
+import com.kobietka.trainingtimer.repositories.ActiveGoalRepository
+import com.kobietka.trainingtimer.repositories.CompletedGoalRepository
 import com.kobietka.trainingtimer.repositories.ExerciseRepository
 import com.kobietka.trainingtimer.repositories.WorkoutRepository
 import dagger.Module
@@ -25,6 +29,18 @@ class RepositoryModule {
     @Singleton
     fun provideWorkoutRepository(workoutDao: WorkoutDao): WorkoutRepository {
         return WorkoutRepository(workoutDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideActiveGoalRepository(activeGoalDao: ActiveGoalDao): ActiveGoalRepository {
+        return ActiveGoalRepository(activeGoalDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCompletedGoalRepository(completedGoalDao: CompletedGoalDao): CompletedGoalRepository {
+        return CompletedGoalRepository(completedGoalDao)
     }
 
 }
