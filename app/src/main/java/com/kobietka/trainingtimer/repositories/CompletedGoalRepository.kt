9481920f42921696e67976f77fb5ea966e7dd65f
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-21 10:12:28 +0100

    Add CompletedGoalRepository

---
+++ app/src/main/java/com/kobietka/trainingtimer/repositories/CompletedGoalRepository.kt
@@ -0,0 +1,26 @@
+package com.kobietka.trainingtimer.repositories
+
+import com.kobietka.trainingtimer.data.CompletedGoal
+import com.kobietka.trainingtimer.data.CompletedGoalDao
+import io.reactivex.Completable
+import io.reactivex.Maybe
+import io.reactivex.Observable
+import javax.inject.Inject
+
+
+class CompletedGoalRepository
+@Inject constructor(private val completedGoalDao: CompletedGoalDao){
+
+    fun insert(completedGoal: CompletedGoal): Completable {
+        return completedGoalDao.insert(completedGoal)
+    }
+
+    fun getById(id: Int): Maybe<CompletedGoal> {
+        return completedGoalDao.getById(id)
+    }
+
+    fun getAllIds(): Observable<List<Int>> {
+        return completedGoalDao.getAllIds()
+    }
+
+}
