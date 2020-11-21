0c28a757473e68ef12387f8c77a0e7c70fd49c28
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-21 10:09:57 +0100

    Add CompletedGoalDao

---
+++ app/src/main/java/com/kobietka/trainingtimer/data/CompletedGoalDao.kt
@@ -0,0 +1,22 @@
+package com.kobietka.trainingtimer.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import io.reactivex.Completable
+import io.reactivex.Maybe
+import io.reactivex.Observable
+
+@Dao
+interface CompletedGoalDao {
+
+    @Insert
+    fun insert(completedGoal: CompletedGoal): Completable
+
+    @Query("SELECT * FROM completedGoal WHERE id = :id")
+    fun getById(id: Int): Maybe<CompletedGoal>
+
+    @Query("SELECT id FROM completedGoal")
+    fun getAllIds(): Observable<List<Int>>
+
+}
