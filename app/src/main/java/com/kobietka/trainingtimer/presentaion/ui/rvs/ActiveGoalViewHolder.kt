e311793dc1689a4777e58a09c3a5f401229ed07e
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-21 10:48:00 +0100

    Add ActiveGoalViewHolder class

---
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/rvs/ActiveGoalViewHolder.kt
@@ -0,0 +1,45 @@
+package com.kobietka.trainingtimer.presentaion.ui.rvs
+
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.RecyclerView
+import com.kobietka.trainingtimer.R
+import com.kobietka.trainingtimer.presentaion.viewmodels.ActiveGoalViewModel
+
+
+class ActiveGoalViewHolder(itemView: View, val viewModel: ActiveGoalViewModel, private val lifecycleOwner: LifecycleOwner) : RecyclerView.ViewHolder(itemView) {
+
+    fun onAttach(){
+        viewModel.name().observe(lifecycleOwner, {
+            itemView.findViewById<TextView>(R.id.active_goal_name).text = it
+        })
+
+        viewModel.type().observe(lifecycleOwner, {
+            itemView.findViewById<TextView>(R.id.active_goal_type).text = it
+        })
+
+        viewModel.attached().observe(lifecycleOwner, {
+            itemView.findViewById<TextView>(R.id.active_goal_attached).text = it
+        })
+
+        viewModel.goal().observe(lifecycleOwner, {
+            itemView.findViewById<TextView>(R.id.active_goal_progress_end).text = it
+            itemView.findViewById<ProgressBar>(R.id.active_goal_progress_bar).max = it.toInt()
+        })
+
+        viewModel.currentProgress().observe(lifecycleOwner, {
+            itemView.findViewById<ProgressBar>(R.id.active_goal_progress_bar).progress = it.toInt()
+        })
+    }
+
+    fun onDetach(){
+        viewModel.name().removeObservers(lifecycleOwner)
+        viewModel.type().removeObservers(lifecycleOwner)
+        viewModel.attached().removeObservers(lifecycleOwner)
+        viewModel.goal().removeObservers(lifecycleOwner)
+        viewModel.currentProgress().removeObservers(lifecycleOwner)
+    }
+
+}
