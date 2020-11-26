b1c540dec2f81ce83058b84564e6335d67de381a
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-26 15:24:49 +0100

    Add WeekDao to AppDatabase

--- app/src/main/java/com/kobietka/trainingtimer/data/AppDatabase.kt
+++ app/src/main/java/com/kobietka/trainingtimer/data/AppDatabase.kt
@@ -9,7 +9,7 @@ import com.kobietka.trainingtimer.models.Converters
 @Database(entities = [WorkoutEntity::class, ExerciseEntity::class,
     WorkoutRelation::class, HistoryEntity::class,
     ActiveGoal::class, CompletedGoal::class,
-    CompletedWorkoutEntity::class], version = 19)
+    CompletedWorkoutEntity::class, WeekEntity::class], version = 19)
 @TypeConverters(Converters::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun workoutDao(): WorkoutDao
@@ -19,4 +19,5 @@ abstract class AppDatabase: RoomDatabase() {
     abstract fun activeGoalDao(): ActiveGoalDao
     abstract fun completedGoalDao(): CompletedGoalDao
     abstract fun completedWorkoutDao(): CompletedWorkoutDao
+    abstract fun weekDao(): WeekDao
 }
