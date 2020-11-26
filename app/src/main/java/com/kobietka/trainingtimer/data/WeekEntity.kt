3816c9452cd765523e3a2edc4e45b9efe9c7deb5
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-26 15:23:57 +0100

    Add WeekEntity class

---
+++ app/src/main/java/com/kobietka/trainingtimer/data/WeekEntity.kt
@@ -0,0 +1,13 @@
+package com.kobietka.trainingtimer.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "week")
+data class WeekEntity(@PrimaryKey(autoGenerate = true) val id: Int?,
+                      @ColumnInfo val isActive: Boolean,
+                      @ColumnInfo val startDay: Int,
+                      @ColumnInfo val startMonth: Int,
+                      @ColumnInfo val startYear: Int)
