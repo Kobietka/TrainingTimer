7954717be07b66bfde3615414829c117ad7f322f
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-26 14:51:27 +0100

    Add CompletedWorkoutEntity class

---
+++ app/src/main/java/com/kobietka/trainingtimer/data/CompletedWorkoutEntity.kt
@@ -0,0 +1,15 @@
+package com.kobietka.trainingtimer.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "completedWorkout")
+data class CompletedWorkoutEntity(@PrimaryKey(autoGenerate = true) val id: Int?,
+                                  @ColumnInfo val weekId: Int?,
+                                  @ColumnInfo val workoutName: String,
+                                  @ColumnInfo val workoutCompletionTime: Int,
+                                  @ColumnInfo val completionDay: Int,
+                                  @ColumnInfo val completionMonth: Int,
+                                  @ColumnInfo val completionYear: Int)
