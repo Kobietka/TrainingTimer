8ec3181003e609dde24fb42a65ad68fbd794a125
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-26 15:25:04 +0100

    Add WeekRepository class

---
+++ app/src/main/java/com/kobietka/trainingtimer/repositories/WeekRepository.kt
@@ -0,0 +1,30 @@
+package com.kobietka.trainingtimer.repositories
+
+import com.kobietka.trainingtimer.data.WeekDao
+import com.kobietka.trainingtimer.data.WeekEntity
+import io.reactivex.Completable
+import io.reactivex.Maybe
+import io.reactivex.Observable
+import javax.inject.Inject
+
+
+class WeekRepository
+@Inject constructor(private val weekDao: WeekDao){
+
+    fun insert(weekEntity: WeekEntity): Completable {
+        return weekDao.insert(weekEntity)
+    }
+
+    fun getById(id: Int): Maybe<WeekEntity> {
+        return weekDao.getById(id)
+    }
+
+    fun getAllIds(): Observable<List<Int>> {
+        return weekDao.getAllIds()
+    }
+
+    fun deleteAll(): Completable {
+        return weekDao.deleteAll()
+    }
+
+}
