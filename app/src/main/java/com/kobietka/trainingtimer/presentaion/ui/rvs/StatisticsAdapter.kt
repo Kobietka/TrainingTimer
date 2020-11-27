4079fbbfc48f3f161e7f026ba219e01bdc983b3a
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-27 12:49:11 +0100

    Add StatisticsAdapter class

---
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/rvs/StatisticsAdapter.kt
@@ -0,0 +1,75 @@
+package com.kobietka.trainingtimer.presentaion.ui.rvs
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.lifecycle.LifecycleOwner
+import androidx.recyclerview.widget.RecyclerView
+import com.kobietka.trainingtimer.R
+import com.kobietka.trainingtimer.presentaion.viewmodels.StatisticsViewModel
+import com.kobietka.trainingtimer.repositories.WeekRepository
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
+import javax.inject.Provider
+
+
+class StatisticsAdapter
+@Inject constructor(private val weekRepository: WeekRepository,
+                    private val viewModelProvider: Provider<StatisticsViewModel>) : RecyclerView.Adapter<StatisticsViewHolder>() {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    private var ids = listOf<Int>()
+    private lateinit var lifecycleOwner: LifecycleOwner
+
+    private fun updateIds(newIds: List<Int>){
+        ids = newIds
+        notifyDataSetChanged()
+    }
+
+    fun setLifeCycleOwner(lifecycleOwner: LifecycleOwner){
+        this.lifecycleOwner = lifecycleOwner
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StatisticsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.recycler_view_statistics_entry, parent, false)
+
+        return StatisticsViewHolder(view, viewModelProvider.get(), lifecycleOwner)
+    }
+
+    override fun onBindViewHolder(holder: StatisticsViewHolder, position: Int) {
+        holder.viewModel.switchId(ids[position])
+    }
+
+    override fun getItemCount(): Int {
+        return ids.size
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        compositeDisposable.add(
+            weekRepository.getAllIds()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::updateIds)
+        )
+    }
+
+    override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView)
+        compositeDisposable.clear()
+    }
+
+    override fun onViewAttachedToWindow(holder: StatisticsViewHolder) {
+        super.onViewAttachedToWindow(holder)
+        holder.onAttach()
+    }
+
+    override fun onViewDetachedFromWindow(holder: StatisticsViewHolder) {
+        super.onViewDetachedFromWindow(holder)
+        holder.onDetach()
+    }
+
+}
