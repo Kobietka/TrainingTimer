13659cc86bebc84785e8af2caa93eb39db17d6fc
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-28 09:21:02 +0100

    Setup recycler view in StatisticsFragment

--- app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/fragments/StatisticsFragment.kt
@@ -3,15 +3,22 @@ package com.kobietka.trainingtimer.presentaion.ui.fragments
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.transition.MaterialContainerTransform
 import com.kobietka.trainingtimer.R
 import com.kobietka.trainingtimer.presentaion.common.BaseFragment
+import com.kobietka.trainingtimer.presentaion.ui.rvs.StatisticsAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_statistics.*
+import javax.inject.Inject
 
 
 @AndroidEntryPoint
 class StatisticsFragment : BaseFragment() {
+
+    @Inject lateinit var adapter: StatisticsAdapter
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +32,17 @@ class StatisticsFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
+
+        adapter.setLifeCycleOwner(viewLifecycleOwner)
+        recyclerView = view.findViewById(R.id.fragment_statistics_rv)
+        recyclerView.layoutManager = LinearLayoutManager(
+            activity,
+            RecyclerView.VERTICAL,
+            false
+        )
+
+        recyclerView.adapter = adapter
+
         fragment_statistics_back_arrow.setOnClickListener {
             requireActivity().onBackPressed()
         }
