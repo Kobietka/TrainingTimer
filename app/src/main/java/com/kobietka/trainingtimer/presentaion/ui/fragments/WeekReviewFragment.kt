1404c7f3c6113465efe4719b33e16067f60e755c
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-28 12:01:45 +0100

    Add WeekReviewFragment class

---
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/fragments/WeekReviewFragment.kt
@@ -0,0 +1,19 @@
+package com.kobietka.trainingtimer.presentaion.ui.fragments
+
+import android.os.Bundle
+import android.view.View
+import com.kobietka.trainingtimer.R
+import com.kobietka.trainingtimer.presentaion.common.BaseFragment
+
+
+class WeekReviewFragment : BaseFragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.fragment_week_review
+    }
+
+}
