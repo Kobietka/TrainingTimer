2ea7e0deb5abc9c3acbf9ce7537bb4ea3c95f4d4
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-29 10:32:38 +0100

    Add notification on week end

--- app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/MainActivity.kt
+++ app/src/main/java/com/kobietka/trainingtimer/presentaion/ui/MainActivity.kt
@@ -1,16 +1,57 @@
 package com.kobietka.trainingtimer.presentaion.ui
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.kobietka.trainingtimer.R
+import com.kobietka.trainingtimer.presentaion.viewmodels.MainActivityViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @Inject lateinit var viewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel.loadWeeks()
+        createNotificationChannel()
+
+        viewModel.setActionOnNewWeek {
+            val builder = NotificationCompat.Builder(this, "main_channel")
+                .setSmallIcon(R.drawable.ic_done)
+                .setContentTitle("Week review")
+                .setContentText("New review is available for week $it")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            with(NotificationManagerCompat.from(this)) {
+                notify(1, builder.build())
+            }
+        }
+
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification Channel"
+            val descriptionText = "Main notification channel"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("main_channel", name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
 }
