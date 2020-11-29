d3140c0e21454c9c74b5e0ad207dbd93f66f4b8b
Sławomir Krupa <slawekgrawgry@gmail.com>
2020-11-29 10:31:40 +0100

    Change database version

--- app/src/main/java/com/kobietka/trainingtimer/data/AppDatabase.kt
+++ app/src/main/java/com/kobietka/trainingtimer/data/AppDatabase.kt
@@ -9,7 +9,7 @@ import com.kobietka.trainingtimer.models.Converters
 @Database(entities = [WorkoutEntity::class, ExerciseEntity::class,
     WorkoutRelation::class, HistoryEntity::class,
     ActiveGoal::class, CompletedGoal::class,
-    CompletedWorkoutEntity::class, WeekEntity::class], version = 21)
+    CompletedWorkoutEntity::class, WeekEntity::class], version = 29)
 @TypeConverters(Converters::class)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun workoutDao(): WorkoutDao
